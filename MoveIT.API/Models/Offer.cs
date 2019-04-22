216a68e6574c66eff5b284e33fcb28dd56fcf869
Magnus Sundström <kmvsundstrom@gmail.com>
2019-04-22 13:59:04 +0200

    Updated offer model

--- MoveIT.API/Models/Offer.cs
+++ MoveIT.API/Models/Offer.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MoveIT.API.Models.Enums;
 
 namespace MoveIT.API.Models
 {
     public class Offer
     {
+        public int Id { get; set; }
+        public Address FromAddress { get; set; }
+        public Address ToAddress { get; set; }
+        public double Distance { get; set; }
+        public double LivingArea { get; set; }
+        public double AuxArea { get; set; }
+        public bool IsPiano { get; set; }
+        public double PriceExcludingVAT { get; set; }
+        public OfferStatus Status { get; set; }
     }
 }
