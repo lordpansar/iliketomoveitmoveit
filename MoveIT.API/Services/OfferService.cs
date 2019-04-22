e58b5b452d6f0fc2057606e912d402c3c792004e
Magnus Sundström <kmvsundstrom@gmail.com>
2019-04-22 10:38:43 +0200

    Added offer service

---
+++ MoveIT.API/Services/OfferService.cs
@@ -0,0 +1,12 @@
+﻿using MoveIT.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveIT.API.Services
+{
+    public class OfferService : IOfferService
+    {
+    }
+}
