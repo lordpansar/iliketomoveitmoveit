a2b80e413ecaa05128e5663dd0f343578844a0f6
Magnus Sundström <kmvsundstrom@gmail.com>
2019-04-22 10:39:17 +0200

    Added interfaces

---
+++ MoveIT.API/Interfaces/IOfferService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveIT.API.Interfaces
+{
+    public interface IOfferService
+    {
+        Offer GetOfferById(int id);
+    }
+}
