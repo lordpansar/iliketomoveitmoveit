381a9172056c2a31c889a0c77d1aa44e61990cd3
Magnus Sundström <kmvsundstrom@gmail.com>
2019-04-22 10:38:53 +0200

    Added offer repo

---
+++ MoveIT.API/Repositories/OfferRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoveIT.API.Repositories
+{
+    public class OfferRepository
+    {
+
+    }
+}
