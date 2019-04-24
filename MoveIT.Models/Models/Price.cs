b8b6a47a8739f2063b6b088f1a48e2445bcba842
Magnus Sundström <kmvsundstrom@gmail.com>
2019-04-24 15:44:56 +0200

    Added price model

---
+++ MoveIT.Models/Models/Price.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoveIT.Models.Models
+{
+    public class Price
+    {
+        public double DistanceCost { get; set; }
+        public double PianoCost { get; set; }
+        public int NoOfCars { get; set; }
+        public double TotalCost { get; set; }
+    }
+}
